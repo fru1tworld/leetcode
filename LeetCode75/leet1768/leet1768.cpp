290966564b482b80f3e952e5a70ebec25a936591
fru1tworld <fruitworld.planet@gmail.com>
2024-12-09 17:56:33 +0900

    update

--- LeetCode75/leet1768/leet1768.cpp
+++ LeetCode75/leet1768/leet1768.cpp
@@ -4,16 +4,16 @@ public:
         int index1 = 0;
         int index2 = 0; 
         string ans = "";
+        
+        int minLen = min(word1.size(), word2.size());
+        int maxLen = max(word1.size(), word2.size());
 
-        int minIndex = min(word1.size(), word2.size());
-        int maxIndex = max(word1.size(), word2.size());
-
-        for(int i=0; i<minIndex; i++){
+        for(int i=0; i<minLen; i++){
             ans += word1[i];
             ans += word2[i];
         }
-        for(int i=minIndex; i<maxIndex; i++){
-            if(word1.size() == maxIndex){
+        for(int i=minLen; i<maxLen; i++){
+            if(word1.size() == maxLen){
                 ans += word1[i];
             }else{
                 ans += word2[i];
